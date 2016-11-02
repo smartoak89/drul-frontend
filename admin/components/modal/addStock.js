f14f6985d722cb5dc9847e7cd5a55540e7bd130c
yura-koderra <yura@koderra.com>
2016-11-02 21:40:34 +0200

    stocks

--- admin/components/modal/addStock.js
+++ admin/components/modal/addStock.js
@@ -1,5 +1,19 @@
 angular.module('admin')
     .controller('addStock',['$uibModalInstance', '$scope',
         function ($uibModalInstance, $scope){
+            $scope.stock = {};
+            $scope.error = null;
 
+            $scope.addSale = function () {
+                if (!isValid() === true) {
+
+                }
+            };
+
+            function isValid () {
+                if (!$scope.stock.name) return $scope.error = 'Введите название акции';
+                if (!$scope.stock.percent) return $scope.error = 'Введите процент скидки';
+                $scope.error = null;
+                return true;
+            }
         }]);
