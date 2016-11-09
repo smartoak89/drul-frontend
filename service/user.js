ada0a1db9bd52941defa01218a41bae9d3237c9b
yura-koderra <yura@koderra.com>
2016-11-09 10:09:04 +0200

    d

--- service/user.js
+++ service/user.js
@@ -38,6 +38,7 @@ angular.module('app')
                 if (!user.email) return callback('Пожалуйста введите email!');
                 if (!reg.test(user.email)) return callback('Некоректный email!');
                 if (!user.phone) return callback('Пожалуйста введите номер телефона!');
+                if (!user.state) return callback('Пожалуйста выберите страну!');
                 if (!user.password) return callback('Пожалуйста введите пароль!');
                 if (user.password.length < 4) return callback('Пароль должен быть не менее 4 символов!');
 
