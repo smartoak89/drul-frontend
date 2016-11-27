7f6c7fe90a9b98f2f66c5989b041c8a2d4c3d75c
Dmitriy Lisenko <kratoc.ua@gmail.com>
2016-11-27 12:49:48 +0200

    'fgfdg'

--- components/common/nav.js
+++ components/common/nav.js
@@ -1,14 +1,14 @@
 angular.module('app')
-    .component('topNav', {
-        templateUrl: "components/common/top-nav.html",
+    .component('nav', {
+        templateUrl: "components/common/nav.html",
         controller: [ function () {
             var self = this;
             $(window).scroll(function () {
                 var scrollh = $(this).scrollTop();
-                    if (scrollh > 310) {
-                        $(".compact1").addClass('fixed');
+                    if (scrollh > 60) {
+                        $(".compact").addClass('fixed');
                     } else {
-                        $(".compact1").removeClass('fixed');
+                        $(".compact").removeClass('fixed');
                     }
 
             });
