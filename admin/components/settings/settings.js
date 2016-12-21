103d0323ff637777507456d1b082b569cb9cad15
Dmitriy Lisenko <kratoc.ua@gmail.com>
2016-12-21 21:33:38 +0200

    'dfsgfd'

--- admin/components/settings/settings.js
+++ admin/components/settings/settings.js
@@ -1,8 +1,9 @@
 angular.module('admin')
     .component('settings', {
         templateUrl: "admin/components/settings/settings.html",
-        controller: ['Currency', 'FileUploader', 'Conf', 'Goods', function(Currency, FileUploader, Conf, Goods) {
+        controller: ['Currency', 'FileUploader', 'Conf', 'Goods', 'HttpResource', 'File', function(Currency, FileUploader, Conf, Goods, HttpResource, File) {
             var self = this;
+            self.slides = [];
             self.fields=[
                 {name: 'Валюта', props: [
                     {prop: 'Надбавка, %', type: 'inp'},
@@ -19,6 +20,29 @@ angular.module('admin')
             self.cur = Currency.getCurrentCourse();
             console.log(self.cur);
 
+            self.getSlider = function(){
+                self.slides = [];
+                HttpResource.query({params1:'files', params2:'78d56607-fc11-41eb-a60d-fcfb3b3cb797'}, function (res) {
+                    _.forEach(res, function(obj){
+                        if(obj.type == 'slide'){
+                            self.slides.push(obj);
+                        }
+                    });
+                    console.log('prodGall ', self.slides);
+                }, function (err) {
+                    console.error('Get gallery => ', err);
+                })
+            };
+
+            self.remove = function (file) {
+                File.remove(file.uuid, function (err, res) {
+                    if (err) return self.error = err;
+                    console.info('file was removed', res);
+
+                    _.remove(self.slides, file);
+                })
+            };
+
             // Uploader
             var uploader = this.uploader = new FileUploader({
                 url: Conf.api_path + '/file/78d56607-fc11-41eb-a60d-fcfb3b3cb797'
@@ -27,18 +51,23 @@ angular.module('admin')
                 _.each(uploader.queue, function(img){
                     img.alias = 'slide';
                     console.log(img);
-                })
+                });
                 console.log(uploader);
 
             };
             uploader.onCompleteAll = function () {
-                console.log('done')
-                self.editMode = false;
+                uploader.queue = [];
+                self.uploading = false;
+                self.getSlider()
             };
-            self.uploadSlide = function(){
-                console.log('+')
+            uploader.uploadSlide = function(){
+                self.uploading = true;
+                console.log(self.uploading);
                 uploader.uploadAll();
-            }
+            };
 
+            self.$onInit = function () {
+                self.getSlider();
+            };
         }]
     });
--- components/front-page/slider.js
+++ components/front-page/slider.js
@@ -1,7 +1,7 @@
 angular.module('app')
     .component('slider', {
         templateUrl: "components/front-page/slider.html",
-        controller: ['$rootScope', function($rootScope) {
+        controller: ['$rootScope', 'HttpResource', function($rootScope, HttpResource) {
             var self = this;
             self.myInterval = 5000;
             self.noWrapSlides = false;
@@ -17,7 +17,28 @@ angular.module('app')
             $rootScope.$on('modalClosed', function () {
                 console.info('modalClosed')
                 self.noWrapSlides = false;
-            })
+            });
+
+            self.getSlider = function(){
+                self.slides = [];
+
+                HttpResource.query({params1:'files', params2:'78d56607-fc11-41eb-a60d-fcfb3b3cb797'}, function (res) {
+                    console.log(res);
+                    _.forEach(res, function(obj){
+                        if(obj.type == 'slide'){
+                            self.slides.push(obj);
+                        }
+                    });
+                    console.log(self.slides);
+                }, function (err) {
+                    console.error('Get gallery => ', err);
+                })
+            };
+
+            self.$onInit = function () {
+                console.log('+')
+                self.getSlider();
+            };
 
         }]
     });
