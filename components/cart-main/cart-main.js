61ed21739148b96278e85a719dcccaf383b702a7
Dmitriy Lisenko <kratoc.ua@gmail.com>
2017-01-28 18:11:03 +0200

    'dsgdfg

--- components/cart-main/cart-main.js
+++ components/cart-main/cart-main.js
@@ -1,11 +1,11 @@
 angular.module('app')
     .component('cartMain', {
-        templateUrl: "components/cart-main/cart-main.html",
+        templateUrl: "components/cart-main/success.html",
         controller: ['Cart', 'User', function(Cart, User) {
             var self = this;
             self.user = User;
             self.cart = Cart;
-            console.log(self.cart.cartList)
+            console.log(self.cart)
             //self.countPlus = function(){
             //    self.counter++;
             //};
