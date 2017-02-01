702efe4e5173b16837a3ed5be481bab0c1942f2a
Dmitriy Lisenko <kratoc.ua@gmail.com>
2017-02-01 23:00:41 +0200

    'sdfdf'

--- components/cart-main/cart-main.js
+++ components/cart-main/cart-main.js
@@ -1,21 +1,26 @@
 angular.module('app')
     .component('cartMain', {
         templateUrl: "components/cart-main/cart-main.html",
-        controller: ['Cart', 'User', 'Product', function(Cart, User, Product) {
+        controller: ['Cart', 'User', 'Product', '$timeout', function(Cart, User, Product, $timeout) {
             var self = this;
             self.user = User;
             self.cart = Cart;
             self.orderInfo = false;
-            self.cart.list().then(function(){
+            if(self.user.active == null){
+                if(self.cart.cartList == null){
+                    self.cart.cartList = [];
+                }
                 for(var i =0; i<self.cart.cartList.length; i++){
                     self.cart.cartList[i].counter = 1;
                     console.log(self.cart.cartList[i]);
                 }
-            });
-            console.log(self.user);
-            self.$onInit = function() {
-                //self.Product.getGallery(self.cartList);
-                //console.log(self.cartList);
+            }else {
+                self.cart.list().then(function () {
+                    for (var i = 0; i < self.cart.cartList.length; i++) {
+                        self.cart.cartList[i].counter = 1;
+                    }
+
+                });
             }
             self.countPlus = function(i){
                 self.cart.cartList[i].counter++;
@@ -29,15 +34,23 @@ angular.module('app')
                     self.cart.cartList[i].counter = 0;
                 }
             };
-            self.makeOrder = function(){
-                self.orderInfo = true;
-                self.count = 0;
-                self.cost = 0;
-                for(var i =0; i<self.cart.cartList.length; i++){
-                    self.count = self.count + self.cart.cartList[i].counter;
-                    self.cost = self.cost + self.cart.cartList[i].price*self.cart.cartList[i].counter;
+            self.clickToMod = function () {
+                $timeout(function() {
+                    angular.element('#toMod').triggerHandler('click');
+                });
+            };
+            self.makeOrder = function() {
+                if (self.user.active == null) {
+                    self.clickToMod();
+                } else {
+                    self.orderInfo = true;
+                    self.count = 0;
+                    self.cost = 0;
+                    for (var i = 0; i < self.cart.cartList.length; i++) {
+                        self.count = self.count + self.cart.cartList[i].counter;
+                        self.cost = self.cost + self.cart.cartList[i].price * self.cart.cartList[i].counter;
+                    }
                 }
             }
-
         }]
     });
