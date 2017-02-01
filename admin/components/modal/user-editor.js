c56ddea5dd70a301cc17d160c3b9a0badb64ca26
smartoak89 <smartoak89@gmail.com>
2017-02-01 19:15:27 +0200

    dlsk

--- admin/components/modal/user-editor.js
+++ admin/components/modal/user-editor.js
@@ -8,7 +8,6 @@ angular.module('app')
                 validate($scope.user, function (err) {
                     if (err) return $scope.error = err;
                     $scope.error = null;
-                    console.log('all right');
                 })
             };
 
@@ -18,12 +17,8 @@ angular.module('app')
 
             function validate (user, callback) {
                 var reg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-                if (!user.firstname) return callback('Пожалуйста введите имя!');
-                if (!user.lastname) return callback('Пожалуйста введите Фамилию!');
                 if (!user.email) return callback('Пожалуйста введите email!');
                 if (!reg.test(user.email)) return callback('Некоректный email!');
-                // if (!user.state) return callback('Выберите страну!');
-                if (!user.phone) return callback('Пожалуйста введите номер телефона!');
 
                 return callback(null);
 
--- admin/components/settings/settings.js
+++ admin/components/settings/settings.js
@@ -4,19 +4,16 @@ angular.module('admin')
         controller: ['Currency', 'Conf', 'Goods', 'HttpResource', 'File', function(Currency, Conf, Goods, HttpResource, File) {
             var self = this;
             self.slides = [];
-            self.fields=[
-                {name: 'Валюта', props: [
-                    {prop: 'Надбавка, %', type: 'inp'},
-                    {prop: 'Валюта по умолчанию', type: 'sel', opt:['UAH', 'RUB', 'DOL']}
-                ]},
-                {name: 'Музыка', props: [
-                    {prop: 'Автовоспроизведние', type: 'check'}
-                ]},
-                {name: 'Доставка', props: [
-                    {prop: 'Цена доставки', type: 'inp'},
-                    {prop: 'Бесплатная доставка от', type: 'inp'}
-                ]}
-            ];
+            // self.fields=[
+            //     {name: 'Валюта', props: [
+            //         {prop: 'Надбавка, %', type: 'inp'},
+            //         {prop: 'Валюта по умолчанию', type: 'sel', opt:['UAH', 'RUB', 'DOL']}
+            //     ]},
+            //     {name: 'Доставка', props: [
+            //         {prop: 'Цена доставки', type: 'inp'},
+            //         {prop: 'Бесплатная доставка от', type: 'inp'}
+            //     ]}
+            // ];
             self.cur = Currency.getCurrentCourse();
         }]
     });
