6758e242ce0a900c0b71650594deca8c5233a698
Dmitriy Lisenko <kratoc.ua@gmail.com>
2017-02-19 12:36:43 +0200

    'sdfdasd'

--- admin/components/orders/order-detale.js
+++ admin/components/orders/order-detale.js
@@ -3,8 +3,12 @@ angular.module('admin')
         templateUrl: "admin/components/orders/order-detale.html",
         controller: ['RequestService', '$location', function(RequestService, $location) {
             var self = this;
-
             var orderID = $location.$$path.split('/').pop();
+
+            self.editUserInfo = false;
+            self.editOrderInfo = false;
+            self.newUserInfo = {};
+            self.newOrderInfo = {};
 
             RequestService.getOneOrder(orderID, function (err, res) {
                 self.order = res;
