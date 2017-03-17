2a13b8a4b205edf4dc042b462af011942f5b4ced
yura <smartoak89@gmail.com>
2017-03-17 05:59:07 +0200

    buy-now

---
+++ components/modal/buy-now.js
@@ -0,0 +1,29 @@
+angular.module('app')
+    .controller('buyNow', ['$uibModalInstance', '$scope', 'Httpquery', 'User', 'product', 'CurrencyService',
+        function ($uibModalInstance, $scope, Httpquery, User, product, CurrencyService) {
+        console.log(product);
+        var order = {
+            price: product.price,
+            currency:  CurrencyService.cy,
+            products:[{
+                productID: product.uuid,
+                combo: product.combo,
+                price: product.price
+            }]
+        };
+
+        $scope.save = function () {
+            order.phone = $scope.number;
+            console.log('$scope.number',order);
+            Httpquery.save({params1: 'order', params2: 'now'}, order, function (res) {
+                console.log('res', res);
+            }, function (err) {
+                console.log('err', err);
+            });
+        };
+
+        $scope.close = function () {
+            $uibModalInstance.dismiss();
+        }
+    }]);
+
