4ee73fa3119afdf25e02b46b170c999ff9914e81
Sorriss <kratoc.ua@gmail.com>
2017-06-01 09:40:01 +0300

    'fdsadsf'

--- admin/components/modal/editCategory.js
+++ admin/components/modal/editCategory.js
@@ -1,15 +1,16 @@
 angular.module('admin')
-    .controller('editCategory',['$uibModalInstance', '$scope', 'Categories', 'HttpResource', 'modalData',
-        function($uibModalInstance, $scope, Categories, HttpResource, modalData){
+    .controller('editCategory',['$uibModalInstance', '$scope', '$state', 'Categories', 'HttpResource', 'modalData',
+        function($uibModalInstance, $scope, $state, Categories, HttpResource, modalData){
             $scope.curCategName = modalData.cellTemplateScope.branch.name;
             $scope.curCategId = modalData.cellTemplateScope.branch.uuid;
             this.$onInit = function () {
                 console.log(modalData.cellTemplateScope.branch)
             }
             $scope.change = function(){
-                HttpResource.put({params1: 'category', params2: $scope.curCategId}, $scope.curCategName, function(res){
+                HttpResource.put({params1: 'category', params2: $scope.curCategId}, {name: $scope.curCategName}, function(res){
                     $uibModalInstance.dismiss('cancel');
-                    $state.reload();
+                    // $state.reload();
+                    modalData.cellTemplateScope.branch.name = $scope.curCategName;
                 }, function(err){
                     $scope.error = err;
                 })
