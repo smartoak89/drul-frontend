f10ae642bbdbc1f2ecffd5d31126b32e0f61dd8d
Sorriss <kratoc.ua@gmail.com>
2017-06-13 01:02:58 +0300

    'sfgdfs'

--- components/cart-main/cart-main.js
+++ components/cart-main/cart-main.js
@@ -1,8 +1,8 @@
 angular.module('app')
     .component('cartMain', {
         templateUrl: "components/cart-main/cart-main.html",
-        controller: ['$rootScope', 'Cart', 'User', '$timeout', '$anchorScroll', 'CurrencyService', 'Conf',
-            function($rootScope, Cart, User, $timeout, $anchorScroll, CurrencyService, Conf) {
+        controller: ['$cookies', '$state', '$rootScope', 'Cart', 'User', '$timeout', '$anchorScroll', 'CurrencyService', 'Conf',
+            function($cookies, $state, $rootScope, Cart, User, $timeout, $anchorScroll, CurrencyService, Conf) {
                 var self = this;
                 self.Conf = Conf;
 
@@ -40,13 +40,15 @@ angular.module('app')
                     if (!self.user) return self.clickToMod();
 
                     $anchorScroll(0);
-                    self.orderInfo = true;
                     self.count = 0;
                     self.cost = 0;
                     for (var i = 0; i < self.cartList.length; i++) {
                         self.count = self.count + self.cartList[i].counter;
                         self.cost = self.cost + self.cartList[i].price * self.cartList[i].counter;
                     }
+
+                    $cookies.put('order', JSON.stringify({order: self.cartList, count: self.count, cost: self.cost, currency: self.currencyService.cy}))
+                    $state.go('index.order');
                 };
 
                 function getList () {
